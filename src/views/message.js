5f085a3da91209d4a81f6ccf24ad111150070e7e
Ashley Pollard <ash.pollard11@yahoo.com>
2020-02-20 13:19:45 -0500

    Migrated html, css and js content into widget framework

--- src/views/message.js
+++ src/views/message.js
@@ -1,5 +1,6 @@
 import html from './message.html';
 import './message.css';
+import { TweenMax, TimelineMax, Linear, Back } from 'gsap/all'
 
 let elements = [];
 let body;
@@ -8,7 +9,11 @@ export function show(text) {
     // convert plain HTML string into DOM elements
     let temporary = document.createElement('div');
     temporary.innerHTML = html;
-    temporary.getElementsByClassName('js-widget-dialog')[0].textContent = text;
+    // temporary.getElementsByClassName('js-widget-dialog')[0].textContent = text;
+
+    const closeButton = temporary.querySelector("#close");
+    const overlay = temporary.querySelector("#overlay");
+    const modal = temporary.querySelector("#modal");
 
     // append elements to body
     body = document.getElementsByTagName('body')[0];
@@ -17,12 +22,39 @@ export function show(text) {
         body.appendChild(temporary.children[0]);
     }
 
-    body.addEventListener('click', close);
+    transitionIn(overlay, modal);
+
+    overlay.addEventListener("click", () => transitionOut(overlay, modal));
+    closeButton.addEventListener("click", () => transitionOut(overlay, modal));
+    // body.addEventListener('click', close);
 }
 
 export function close() {
     while (elements.length > 0) {
         elements.pop().remove();
     }
-    body.removeEventListener('click', close);
+    // body.removeEventListener('click', close);
+}
+
+export function transitionIn(overlay, modal) {
+    TweenMax.fromTo(
+        overlay, 0.75,
+        { opacity: 0},
+        { opacity: 1, ease: Linear.easeNone, delay: 0.5}
+    );
+    TweenMax.fromTo(
+        modal, 0.75,
+        { y: (0 - body.clientHeight)},
+        { y: 0, ease: Back.easeOut.config(0.1), delay: 0.5}
+    );
+}
+
+export function transitionOut(overlay, modal)  {
+    TweenMax.to(overlay, 0.4, {opacity: 0, ease: Linear.easeNone, delay: 0.3,
+        onComplete: () => { overlay.style.display = "none"; }});
+    TweenMax.to(modal, 0.4, {scale: 0.65, ease: Back.easeIn.config(0.75), 
+        onComplete: () => { 
+            modal.style.display = "none"; 
+            close();
+    }});
 }
